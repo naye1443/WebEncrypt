470758eae3e40910cf10abee5c895b7ce32f8ab3
Eyan Eubanks <eyaneubanks@Eyans-MacBook-Pro.local>
2023-06-21 19:38:31 -0400

    Begining formulation of Encrypt function: Add React to project: Extend next/babel module in .eslintc.json

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, request, send_file, jsonify, render_template, abort
 
 app = Flask(__name__)
 
@@ -6,5 +6,31 @@ app = Flask(__name__)
 def home():
     print("We are at home!")
     return"<h1> Home Page</h1>"
+
+# Description: Encrypts a message
+# Input: message in plain txt. The message will be encoded in UTF-8
+# Output: message as cipher text. The message will be encoded in UTF-8
+@app.route('/encrypt', methods=['POST'])
+def Encrypt():
+
+    try:
+        # Tries to access request of form
+        for key in request.form:
+            
+    
+        if(request.form.get('scheme') == 'elgamal'):
+            # use elgamal Encryption scheme
+        elif(request.form.get('scheme') == 'des'):
+            # use des Encryption scheme
+
+        # Return success Resposnse
+        return {'status': ' ', 'message': ''}
+
+    except Exception as ex:
+        print(f'Exception occurred: {ex}')
+
+        return {'status': 'error', 'message': str(ex)}
+
+
 
 app.run(debug=True, host='127.0.0.1', port='5000')
