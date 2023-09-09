fcebba71fc03bebfd44585a7b9c572a62dac79ce
Eyan Eubanks <eyaneubanks@gmail.com>
2023-09-08 20:52:55 -0700

    Add Setters to DES_2 codebase. Reason is so the attributes access does not need to be changed from protected to public. These attributes need to be exposed to pybind11 to complete DES bindings.

--- DES_2/include/DES.hpp
+++ DES_2/include/DES.hpp
@@ -48,6 +48,41 @@ public:
 
 		std::string F(std::string subkey, std::string right_block);	// is F function for DES
 
+		//** Getter and Setters for python Bindings
+		const int* get_pc1(void){return pc_1;}
+		int* get_numleftShift(void){return num_leftShift;}
+		int* get_numrightShift(void){return num_rightShift;}
+		const int* get_pc2(void){return pc_2;}
+		const int* get_IPt(void){return IP_t;}
+		const int* get_Et(void){return E_t;}
+
+		const int*** get_S(void){
+
+			int numOfBoxes = sizeof(S)/sizeof(S[0]);	// number of S boxes = 8
+			int rowOfPerm = sizeof(S[0])/sizeof(S[0][0]);	// rows of permutations = 4
+			int colOfPrem = sizeof(S[0][0])/sizeof(S[0][0][0]);	// columns of permutations = 16
+
+			int*** arr = new int** [sizeof(S)/sizeof(S[0])];	// pointer that points to a pointer of an array of integers. Adds 8 "boxes" to Sbox container
+
+			for(int i = 0; i < numOfBoxes; i++){	
+				arr[i] = new int* [rowOfPerm]; // Pointer that points to "boxes" equals a pointer that points to 4 arrays of integers
+				for(int j = 0; j < rowOfPerm; j++){
+					arr[i][j] = new int[colOfPrem];	// row pointer that points to 4 arrays of integers equals a array of 16 integers
+					for(int k = 0; k < colOfPrem; k++)
+						arr[i][j][k] = S[i][j][k];	// then fill new array with original permutations
+				}
+			}
+
+			return arr;
+		}
+
+		const int* get_P(void){return P;}
+		const int* get_P1(void){return P_1;}
+		const std::vector<std::string> getCipBlock(void){return cipherBlock;}
+		const std::vector<std::string> getplainBlock(void){return PlainBlock;}
+		const int getBlocks(void){return Blocks;}
+
+
 protected:
 		// This is the PC_1 vector
 	const int pc_1[56] = {  57 ,49 ,41 ,33 ,25 ,17 ,9  ,
