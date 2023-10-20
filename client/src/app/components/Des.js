2122eca37cad74deec893dbd4660e3e66ac3bf1b
Eyan Eubanks <eyaneubanks@gmail.com>
2023-10-19 22:26:47 -0700

    Add Context for persistance for form parameters. Add call to backend sever Migrated parameters to parent page from des.js

--- client/src/app/components/Des.js
+++ client/src/app/components/Des.js
@@ -1,82 +1,75 @@
 'use client'
 
-/*TODO
-
-- Add option for use to add key or to have a random 64-bit key generated for them.
-    - This key generation should be processed on server and returned back to user.
-    - Precursor: - Add Key 64-bit key generation to server side
-
-- Create python modules for different block cipher modes.
-    - These modules will be implmented in seperate python files. One file containing different Block cipher modes.
-    (Note) Other modules will be created to implement other symmetric/asymmetric encryption methods with their respective modes.
-
-- Use modules to handle different client request for encryption/Decryption.
-
-*/
-
-import React, {createContext , useState, useEffect} from 'react';
-import axios from 'axios';
+import {React, useContext} from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import { FormLabel, RadioGroup, Container} from '@mui/material';
+import { FormLabel, RadioGroup, Container, Stack, Tooltip} from '@mui/material';
 import Radio from '@mui/material/Radio';
-import { spacing } from '@mui/system';
-
+import {Context} from './Context';
 
 function Des(props){
 
-    const {value, index}= props
+    const {value ,index} = props
 
-    const [mode, setMode] = useState('Encrypt');
-    const [radio, setRadio] = useState('ofb');
-    const [Text, setText] = useState('');
+    const {
+        EncryptOrDcrypt, setEncryptOrDcrypt,
+        scheme, setScheme,
+        key, setKey,
+        CipherMode, setCipherMode,
+    } = useContext(Context)
 
     // Toggles Encryption/Decryption
-    const toggleMode = () => {
-        mode == 'Encrypt' ? setMode('Decrypt') : setMode('Encrypt');
-    }
-
-    const Submit = (e) => {
-        e.preventDefault();
-
-        console.log("We in this bit");
-
-        const form = new FormData();
-        form.append("Mode", mode.toString());
-        form.append("Radio", radio.toString());
-        form.append("Text", Text.toString());
-
-        const config = {
-            method: 'POST',
-            url: 'http://127.0.0.1:5000/encrypt',
-            data: form
-        }
-        axios(config).then((res) => console.log(res));
+    const toggleEncryptOrDcrypt = () => {
+        EncryptOrDcrypt == 'Encrypt' ? setEncryptOrDcrypt('Decrypt') : setEncryptOrDcrypt('Encrypt');
     }
 
     return(
         <>
             { value === index && (
-            <FormControl onSubmit={Submit} >
+            <FormControl>
                 <Container sx={{mb: 2}}>
                     <FormLabel id="DES-BCM-control" >Encrypt/Decrypt</FormLabel>
                 </Container>
+                <Stack
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={1}>
+                    <Container >
+                        <Button variant="contained" onClick={toggleEncryptOrDcrypt}>{scheme}</Button>
+                    </Container>
+                    <Container >
+                        <Tooltip title=
+                        {<p> If left empty, a randomly generated key will be applied</p>}
+                        placement="top-start"
+                        >
+                            <TextField
+                            id="filled-basic"
+                            label="plaintext"
+                            variant="filled"
+                            multiline
+                            minRows={1}
+                            helperText="Enter key here"
+                            value={key} // Used to persist key when changing tabs
+                            onChange={event => setKey(event.target.value)}    
+                            />
+                        </Tooltip>
+                    </Container>
+                </Stack>
                 <Container sx={{mb: 2}}>
-                    <Button variant="contained" onClick={toggleMode} onChange={(e) => setMode(e.target.value)}>{mode}</Button>
-                </Container>
                     <FormLabel id="demo-radio-buttons-group-label">Block Cipher Mode</FormLabel>
                     <RadioGroup 
                         defaultValue="ofb"
-                        onChange={(e) => setRadio(e.target.value)}
+                        onChange={event => setCipherMode(event.target.value)}
                         row={true}
                     >
                         <FormControlLabel value="ofb" control={<Radio />} label="Output Feedback" />
                         <FormControlLabel value="cm" control={<Radio/>} label="Counter" />
                         <FormControlLabel value="cfm" control={<Radio/>} label="Cipher Feedback" />
                     </RadioGroup>
+                </Container>
             </FormControl>
             )}
         </>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,71 +1,98 @@
 "use client"
 
-import {useState, useEffect, useContext } from 'react'
-import Image from 'next/image';
+import {useState, useEffect} from 'react'
 import styles from './page.module.css';
-import DES from './components/Des';
 import {Container, Stack, TextField, Button, Tab, Tabs} from '@mui/material'
+import DES from './components/Des';
 import Elgamal from './components/Elgamal';
+import {Context} from './components/Context';
+import axios from 'axios';
 
 export default function Home() {
 
-  const [scheme, setScheme] = useState('DES');
-  const [value, setValue] = useState(0);
+  const [scheme, setScheme] = useState('DES');  // used to change tab and select scheme
+  const [EncryptOrDcrypt, setEncryptOrDcrypt] = useState ('Encrypt')
+  const [key, setKey] = useState ('');
+  const [CipherMode, setCipherMode] = useState('OFB');
   const [Text, setText] = useState('');
+
+  // Determines active tab
+  const [SchemeTab, setSchemeTab] = useState(0);
+
+  // Handle changing tab
+  const handleChange = (event, newvalue) => { setSchemeTab(newvalue)};
+
+  // Used to send context to Des Tab
+  const DesForm = {
+    EncryptOrDcrypt, setEncryptOrDcrypt,
+    scheme, setScheme,
+    key, setKey,
+    CipherMode, setCipherMode,
+  }
 
   useEffect(()=>{
     console.log("effect called");
   }, [scheme])
 
-  const renderScheme = ()=>{
-    return (
-    scheme == "DES" ? <DES></DES>: <Elgamal></Elgamal>);
-    
-  }
+  const Submit = (e) => {
+    e.preventDefault();
 
-  // Handle changing tab
-  const handleChange = (event, newvalue) => { setValue(newvalue)};
+    const form = new FormData();
+    form.append("EncryptOrDcrypt", EncryptOrDcrypt.toString());
+    form.append("CipherMode", CipherMode.toString());
+    form.append("key", key.toString());
+    form.append("Scheme", scheme.toString());
+
+    const config = {
+        method: 'POST',
+        url: 'http://127.0.0.1:5000/encrypt',
+        data: form
+    }
+    axios(config).then((res) => console.log(res));
+  }
   
   return (
     <main className={styles.main}>
-      <div>Test div</div>
-      <Container >
-        <box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="DES"></Tab>
-            <Tab label="Elgamal"></Tab>
-          </Tabs>
-        </box>
-        <Container maxWidth="lg">
-          <DES value={value} index={0}></DES>
-          <Elgamal value={value} index={1}></Elgamal>
-        </Container>
-          <Container>
-            <Stack spacing={2}>
-              <TextField
-                  id="filled-basic"
-                  label="plaintext"
-                  variant="filled"
-                  multiline
-                  minRows={4}
-                  helperText="Plaintext here"
-                  onChange={(e) => setText(e.target.value)}
-              />
-                <TextField
-                  id="filled-basic"
-                  label="plaintext"
-                  variant="filled"
-                  multiline
-                  minRows={4}
-                  helperText="Plaintext here"
-                  onChange={(e) => setText(e.target.value)}
-              />
-              <Container>
-                <Button variant="contained" type='submit' >Encrypt</Button>
-              </Container>
-            </Stack>
+      <Context.Provider value={DesForm}>
+        <div><h1>EncryptWeb</h1></div>
+        <Container >
+          <box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={SchemeTab} onChange={handleChange}>
+              <Tab label="DES"></Tab>
+              <Tab label="Elgamal"></Tab>
+            </Tabs>
+          </box>
+          <Container maxWidth="lg" sx={{ display:'flex',alignContent: "center", justifyContent: "center"}}>
+            <DES value={SchemeTab} index={0}></DES>
+            <Elgamal value={SchemeTab} index={1}></Elgamal>
           </Container>
-      </Container>
+            <Container>
+              <Stack spacing={2}>
+                <TextField
+                    id="filled-basic"
+                    label="plaintext"
+                    variant="filled"
+                    multiline
+                    minRows={4}
+                    helperText="Plaintext here"
+                    onChange={(e) => setText(e.target.value)}
+                />
+                  <TextField
+                    id="filled-basic"
+                    label="plaintext"
+                    variant="filled"
+                    multiline
+                    minRows={4}
+                    helperText="Plaintext here"
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <Container>
+                  <Button variant="contained" type='submit' onClick={Submit}>Encrypt</Button>
+                </Container>
+              </Stack>
+            </Container>
+        </Container>
+      </Context.Provider>
     </main>
   )
 }
